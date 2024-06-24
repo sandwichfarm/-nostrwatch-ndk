ed4c081d5a098bb1a92b224f0df2918439ad72bd
dskvr <dskvr@users.noreply.github.com>
2024-06-24 13:43:20 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { EventGeoCoded } from './kinds/geocoded';
+export { RelayDiscovery } from './kinds/relay-discovery';
+export { RelayMeta } from './kinds/relay-meta';
+export { RelayMonitor } from './kinds/relay-monitor';
